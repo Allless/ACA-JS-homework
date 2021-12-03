947c026a66ab389e696b16f7e8e03a56671fe181
Hovhannes Sergoyan <the.frost000@gmail.com>
2021-12-03 17:36:38 +0400

    Deleted uppercase files

--- Homework #5/Image Uploader/Script.js
+++
@@ -1,9 +0,0 @@
-let uploadButton = document.getElementById('upload');
-let img = document.getElementById('output');
-
-function handleFileLoad({ target }) {
-    console.log(target.files[0], URL.createObjectURL(target.files[0]));
-    img.src = URL.createObjectURL(target.files[0]);
-}
-
-uploadButton.addEventListener('change', handleFileLoad);
