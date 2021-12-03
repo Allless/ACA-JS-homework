795bf37b319a7cd4f32414453d657b719a5a6534
Hovhannes Sergoyan <the.frost000@gmail.com>
2021-12-03 17:23:11 +0400

    Filename first letter case changed

---
+++ Homework #5/Counter/script.js
@@ -0,0 +1,33 @@
+let counter = document.getElementById('counter');
+let decreaseButton = document.getElementById('dec');
+let resetButton = document.getElementById('res');
+let increaseButton = document.getElementById('inc');
+if (localStorage) {
+    if (!localStorage['counter']) {
+        localStorage['counter'] = 0;
+    }
+    updateCounter();
+
+    function updateCounter() {
+        counter.innerText = localStorage['counter'];
+    }
+
+    decreaseButton.addEventListener('click', () => {
+        if (counter.innerText != '0') {
+            localStorage['counter'] -= 1;
+            updateCounter();
+        }
+    });
+
+    increaseButton.addEventListener('click', () => {
+        localStorage['counter'] -= -1;
+        updateCounter();
+    });
+
+    resetButton.addEventListener('click', () => {
+        localStorage['counter'] = 0;
+        updateCounter();
+    });
+} else {
+    counter.innerText = 'Ooops... Your local storage is unavailable';
+}
