d3f3b7757748799e22e7509e4c45b03f86eb6d7e
Hovhannes Sergoyan <the.frost000@gmail.com>
2021-12-05 11:34:00 +0400

    I've tried so hard

---
+++ Classwork #13/Try Hard/script.js
@@ -0,0 +1,65 @@
+class myPromise {
+    constructor(callback, parent = null) {
+        this.status = 'pending';
+        this.promiseQueue = [];
+        this.handlerQueue = [];
+        this.parent = parent;
+
+        this.resolve = this.resolve.bind(this);
+        this.reject = this.reject.bind(this);
+        this.handleResolve = this.handleResolve.bind(this);
+        this.handleReject = this.handleReject.bind(this);
+
+        if (this.parent) {
+            this.cb = function (data) {
+                try {
+                    this.resolve(callback(data));
+                } catch {
+                    this.reject(callback(data));
+                }
+            };
+        } else {
+            callback(this.resolve, this.reject);
+        }
+    }
+    resolve(value) {
+        this.status = 'fulfill';
+        this._data = value;
+        setTimeout(this.handleResolve, 0);
+    }
+    reject(value) {
+        this.status = 'reject';
+        this._data = value;
+        setTimeout(this.handleReject, 0);
+    }
+    then(cb) {
+        let next = new myPromise(cb, this);
+        this.promiseQueue.push(next);
+        return next;
+    }
+    catch(cb) {
+        let handler = new myPromise(cb, this);
+        this.handlerQueue.push(handler);
+
+        return handler;
+    }
+    handleResolve() {
+        for (let promise of this.promiseQueue) {
+            promise.cb(this._data);
+        }
+    }
+    handleReject() {
+        let alone = !Boolean(
+            this.handlerQueue.length + this.promiseQueue.length
+        );
+        for (let handler of this.handlerQueue) {
+            handler.cb(this._data);
+        }
+        for (let promise of this.promiseQueue) {
+            promise.reject(this._data);
+        }
+        if (alone) {
+            throw new Error(this._data);
+        }
+    }
+}
